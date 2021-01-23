a0982632b684d7efabcf937bdeed62ef479849b1
bryan costanich <b@wowzer.net>
2021-01-23 14:08:38 -0800

    Initial stub for NT3Hxxx1

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Radio.Nfc.Nt3hxxx1/Driver/Sensors.Radio.Nfc.Nt3hxxx1/Nt3hxxx1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Meadow.Hardware;
+
+
+namespace Meadow.Foundation.Sensors.Radio.Nfc
+{
+    public class Nt3hxxx1
+    {
+        protected II2cBus bus;
+        protected const byte address = 0x55;
+
+        private Nt3hxxx1() { }
+
+        public Nt3hxxx1(II2cBus i2c)
+        {
+            bus = i2c;
+            Init();
+        }
+
+        protected void Init()
+        {
+            bus.WriteData(address, 0x00);
+            byte[] read = bus.ReadData(0x55, 16);
+            foreach (byte b in read) {
+                Console.Write($"{b:X},");
+            }
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Radio.Nfc.Nt3hxxx1/Samples/Sensors.Radio.Nfc3xxx1_Sample/MeadowApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+using Meadow.Foundation.Sensors.Radio.Nfc;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        protected Nt3hxxx1 nfc;
+        protected II2cBus i2c;
+
+        public MeadowApp()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+            i2c = Device.CreateI2cBus(I2cBusSpeed.Standard);
+            nfc = new Nt3hxxx1(i2c);
+        }
+
+    }
+}
